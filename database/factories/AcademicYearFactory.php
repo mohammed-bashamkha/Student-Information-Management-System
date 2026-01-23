b3fbd2ba5196a368adce354fe5e034cb6d6fca04
Mohammed Bashamkha <mb877402@gmail.com>
2026-01-23 21:20:53 +0300

    create factories for models

---
+++ database/factories/AcademicYearFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AcademicYear>
+ */
+class AcademicYearFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'year' => $this->faker->year() . ' - ' . ($this->faker->year() + 1),
+            'start_date' => $this->faker->date(),
+            'end_date' => $this->faker->date(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\AcademicYear;
+use App\Models\Level;
+use App\Models\SchoolClass;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +20,42 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        AcademicYear::factory()->createMany([
+            ['year' => '2022-2023'],
+            ['year' => '2023-2024'],
+            ['year' => '2024-2025'],
+            ['year' => '2025-2026'],
+            ['year' => '2026-2027'],
+        ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Mohammed Faiz Bashamkha',
+            'email' => 'mohammed.bashamkha@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'admin'
+        ]);
+
+        $user_id = User::first('id');
+
+        Level::factory()->createMany([
+            ['created_by' => $user_id,'name' => 'أساسي'],
+            ['created_by' => $user_id,'name' => 'ثانوي'],
+        ]);
+
+        SchoolClass::factory()->createMany([
+            ['created_by' => $user_id,'name' => 'الأول', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'الثاني', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'الثالث', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'الرابع', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'الخامس', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'السادس', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'السابع', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'الثامن', 'level_id' => 1],
+            ['created_by' => $user_id,'name' => 'التاسع', 'level_id' => 1],
+
+            ['created_by' => $user_id,'name' => 'أولى ثانوي', 'level_id' => 2],
+            ['created_by' => $user_id,'name' => 'ثانية ثانوي', 'level_id' => 2],
+            ['created_by' => $user_id,'name' => 'ثالثة ثانوي', 'level_id' => 2],
         ]);
     }
 }
